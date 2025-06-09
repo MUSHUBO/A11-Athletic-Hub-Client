a9e63699995ffe7a04b160fce4562bced6a29ea8
MUSHUBO <shubo3132@gmail.com>
2025-06-10 03:35:05 +0600

    Added The Footer Section With Responsive And Beautiful Design

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -14,26 +14,19 @@ const Navbar = () => {
             Events
         </NavLink>
         <NavLink to="/create-event" className={({ isActive }) => isActive ? "link active" : "link"}>
-            Create Event
+            Create event
+        </NavLink>
+        <NavLink to="/book-event" className={({ isActive }) => isActive ? "link active" : "link"}>
+            Book event
         </NavLink>
         <NavLink to="/myBookings" className={({ isActive }) => isActive ? "link active" : "link"}>
-            My Bookings
+            My bookings
         </NavLink>
     </>
 
     return (
         <div className="navbar w-11/12 mx-auto p-4">
             <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        {links}
-                    </ul>
-                </div>
                 <div className='flex gap-1.5 items-center'>
                     <img className='w-8 h-8' src={navLogo} alt="" />
                     <div className='font-bold'>
@@ -42,18 +35,40 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
+
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     {links}
                 </ul>
             </div>
+
             <div className="navbar-end flex gap-5">
-                <img className='w-11 h-11' src={avatar} alt="" />
+                {/* avatar dropdown section */}
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="Tailwind CSS Navbar component" src={avatar} />
+                        </div>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-6 w-52 p-2 shadow">
+                        <li>
+                            {links}
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+
+                {/* login logOut button */}
                 <Link to="/auth/login">
                     <button className='btn btn-outline btn-primary'>LogIn</button>
                 </Link>
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -6,6 +6,8 @@ import AuthLayout from "../layouts/AuthLayout";
 import Login from "../features/auth/Login";
 import Register from "../features/auth/Register";
 import NotFound from "../pages/Error/NotFound";
+import Terms from "../pages/Terms&Privacy/Terms";
+import Privacy from "../pages/Terms&Privacy/Privacy";
 
 const router = createBrowserRouter([
     {
@@ -26,8 +28,20 @@ const router = createBrowserRouter([
                 element: <div>Create Event</div>
             },
             {
+                path: '/book-event',
+                element: <div>book event</div>
+            },
+            {
                 path: '/myBookings',
                 element: <div>My Bookings</div>
+            },
+            {
+                path: '/terms',
+                Component: Terms
+            },
+            {
+                path: '/privacy',
+                Component: Privacy
             },
         ]
     },
